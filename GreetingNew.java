05880cc34d650db5c561bc6eaca0dc918ca96004
wjehd375 <129487546+wjehd375@users.noreply.github.com>
2023-03-31 17:15:23 +0900

    Update GreetingNew.java

--- GreetingNew.java
+++ GreetingNew.java
@@ -13,6 +13,6 @@ public class GreetingNew
    public static void main (String[] args)
    {
 	System.out.println("Hello Jeon Jeon Dong Dong!");
-	System.out.println("Hello Park Chanho!");   
+	System.out.println("Hello Park Chanho!!");   
    }
 }
